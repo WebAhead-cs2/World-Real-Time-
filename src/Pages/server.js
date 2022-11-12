6e4565912c11e174f388d768612ffaf4ddc46862
LeenMass <leen.msarwy12@gmail.com>
2022-11-12 13:32:02 +0200

    Pages removed

--- src/Pages/server.js
+++
@@ -1,8 +0,0 @@
-
-const http= require('http');
-const router=require('./router')
-const server=http.createServer(router);
-
-const port=3000;
-
-server.listen(port,()=>console.log(`Listening on http://localhost:${port}`))
